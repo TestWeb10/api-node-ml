ecd14cd354d8a991b71ca1bbc0836ff9e82096c6
TestWeb10 <testweb1379@gmail.com>
2024-10-16 17:53:01 -0600

    Se agrego validacion para categoria sat (build dist)

--- dist/db/dbConnection.js
+++ dist/db/dbConnection.js
@@ -6,10 +6,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var dbConnection;
 module.exports = {
   connectToDb: function connectToDb(callback) {
-    _mongoose["default"].createConnection(_config["default"].URI_DB_CONNECTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }).asPromise().then(function (conn) {
+    _mongoose["default"].createConnection(_config["default"].URI_DB_CONNECTION).asPromise().then(function (conn) {
       dbConnection = conn;
       console.log("Database is connected");
       return callback();
